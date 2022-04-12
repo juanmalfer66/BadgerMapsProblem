25f4e7fcbf2c5329a86924ea48f49748211e8343
juanmalfer66 <juanmalfer.projects@gmail.com>
2022-04-12 19:45:07 +0200

    Actualizacion

--- BadgerMapsProblem.py
+++ BadgerMapsProblem.py
@@ -23,6 +23,36 @@ def sinAcentos(word):
                          unicodedata.normalize('NFKD', word).translate(trans_tab))  
     return(word_wto_accent)
 
+# Obtiene la una lista con todos los nombres
+def get_full_names(csv_list,header):
+    names = list()
+    for i in range(len(csv_list)):
+        if (csv_list[i][header['First Name']] == '') and (csv_list[i][header['Last Name']] == ''):
+            names.append('')
+        else:
+            names.append(csv_list[i][header['First Name']] + " " + 
+                         csv_list[i][header['Last Name']])
+    return names
+
+# Devuelve una lista de datos a partir de unos parametros de entrada requeridos
+def get_Required_Data(csv_list,header,required_fields):
+    required_data = list()
+    for i in range(len(csv_list)):
+        aux = list()
+        for field in required_fields:
+            aux.append(csv_list[i][header[field]])
+        required_data.append(aux)
+    return required_data
+
+# Salta una excepción al encontrar un campo vacío
+def empty_field(used_list,position):
+    # Si divide entre 0, salta la excepción al estar el campo vacio
+    try:
+        1/len(used_list[position])
+    except:
+        print("NOT KNOWN") 
+
+
 
 csv_file = 'sample test file - sheet1.csv'
 csv_list = csv_to_list(csv_file)
@@ -38,5 +68,35 @@ names = get_full_names(csv_list,header)
 # Se eliminan las tiles para poder ordenarlo alfabeticamente
 
 sorted_names = sorted(names, key = sinAcentos)  
+
+
+# Los datos requeridos son los siguientes
+required_fields = ['First Name', 'Last Name', 'Street', 'Zip', 'City', 
+                   'Last Check-In Date', 'Company']
+
+# Se obtienen los datos relevantes del usuario
+costumer_data = get_Required_Data(csv_list,header,required_fields)
+
+# Obtener check-in date más antiguo
+
+date_list = list()
+for i in range(len(csv_list)):
+    if csv_list[i][header['Last Check-In Date']] == '' :
+        date_list.append('')
+    else:
+        date_list.append(datetime.strptime(
+            csv_list[i][header['Last Check-In Date']],"%d/%m/%Y").timestamp())
+
+
+print("\nLISTA DE LOS CLIENTES ORDENADOS ALFABÉTICAMENTE: \n")
+
+for i in range(len(names)):
+    if len(names[i]) == 0:
+        # Llama a la excepción
+        empty_field(names,i)
+    else:
+        print(names[i])
+
+
 
 
